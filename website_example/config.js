578159cec089ea38f65edda51f2bf2b791e30958
Bill Aue <bill.aue@gmail.com>
2017-03-14 15:17:52 +0700

    Update new difficult target and link format to block explorer

--- website_example/config.js
+++ website_example/config.js
@@ -10,8 +10,8 @@ var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
-var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
+var blockchainExplorer = "http://explorer.sumokoin.com/block/{id}";
 
-var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+var transactionExplorer = "http://explorer.sumokoin.com/tx/{id}";
 
 var themeCss = "themes/default-theme.css";
